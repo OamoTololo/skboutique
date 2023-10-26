165dbbe8adaeba611b64d4c206b90e70b2dd5a77
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2023-10-26 22:54:09 +0200

    Project setup with depemdecies.

---
+++ index.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SkBoutique</title>
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/skboutique.css">
+</head>
+<body>
+    <!----Navbar----->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SKBoutique<i class="fa-solid fa-shopping-bag"></i></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="">
+                <button type="button" class="btn">
+                    <i class=""></i>
+                </button>
+            </div>
+        </div>
+    </nav>
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar----->
+    <!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar-----><!----Navbar----->
+    <!----End of Navbar----->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/skboutique.js"></script>
+</body>
+</html>
